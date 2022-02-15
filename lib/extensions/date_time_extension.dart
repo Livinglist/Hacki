d7332e5b1d5fb4412d41aba0a49bfd1403a36ef2
Livinglist <georgefung78@live.com>
2022-02-15 08:46:09 -0800

    fixed date time extension.

--- lib/extensions/date_time_extension.dart
+++ lib/extensions/date_time_extension.dart
@@ -8,8 +8,11 @@ extension DateTimeExtension on DateTime {
     } else if (diff.inDays > 30) {
       final gap = (now.month - month).clamp(1, 12);
       return '$gap month${gap == 1 ? '' : 's'} ago';
-    } else if (diff.inDays > 1) {
-      return '${diff.inDays} days ago';
+    } else if (diff.inDays >= 1) {
+      if (diff.inHours <= 24) {
+        return '${diff.inHours} hour${diff.inHours == 1 ? '' : 's'} ago';
+      }
+      return '${diff.inDays} day${diff.inDays == 1 ? '' : 's'} ago';
     } else if (diff.inHours >= 1) {
       return '${diff.inHours} hour${diff.inHours == 1 ? '' : 's'} ago';
     } else if (diff.inMinutes >= 1) {
