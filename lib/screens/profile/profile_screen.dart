913be9fc5d38f80576864cb84ab7e16cf3fc50a8
Jiaqi Feng <georgefung78@live.com>
2022-05-27 16:18:36 -0700

    v0.2.11 (#27)
    * fixed pinned story tile.
    
    * bumped version.

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -390,7 +390,7 @@ class _ProfileScreenState extends State<ProfileScreen>
                                   showAboutDialog(
                                     context: context,
                                     applicationName: 'Hacki',
-                                    applicationVersion: 'v0.2.10',
+                                    applicationVersion: 'v0.2.11',
                                     applicationIcon: ClipRRect(
                                       borderRadius: const BorderRadius.all(
                                         Radius.circular(12),
