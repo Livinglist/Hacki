0ae9731ec89820236c4df38e2eee812255c02516
Livinglist <georgefung78@live.com>
2022-06-21 14:27:14 -0700

    bumped version.

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -409,7 +409,7 @@ class _ProfileScreenState extends State<ProfileScreen>
                                   showAboutDialog(
                                     context: context,
                                     applicationName: 'Hacki',
-                                    applicationVersion: 'v0.2.20',
+                                    applicationVersion: 'v0.2.21',
                                     applicationIcon: ClipRRect(
                                       borderRadius: const BorderRadius.all(
                                         Radius.circular(12),
