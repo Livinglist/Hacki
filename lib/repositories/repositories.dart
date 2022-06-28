bf6a5667dc0773a8ee2e1b38a75b53a7f2897192
Livinglist <georgefung78@live.com>
2022-06-28 12:01:36 -0700

    fixed naming.

--- lib/repositories/repositories.dart
+++ lib/repositories/repositories.dart
@@ -1,5 +1,5 @@
 export 'auth_repository.dart';
-export 'cache_repository.dart';
+export 'offline_repository.dart';
 export 'post_repository.dart';
 export 'preference_repository.dart';
 export 'search_repository.dart';
