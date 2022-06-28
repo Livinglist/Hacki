941f7c6d8a89b10c2c4ccf7d07c6dd581dfa645f
Livinglist <georgefung78@live.com>
2022-06-28 16:32:51 -0700

    reverted changes.

--- lib/cubits/time_machine/time_machine_cubit.dart
+++ lib/cubits/time_machine/time_machine_cubit.dart
@@ -10,37 +10,28 @@ part 'time_machine_state.dart';
 class TimeMachineCubit extends Cubit<TimeMachineState> {
   TimeMachineCubit({
     SembastRepository? sembastRepository,
-    StoriesRepository? storiesRepository,
     CommentCache? commentCache,
   })  : _sembastRepository =
             sembastRepository ?? locator.get<SembastRepository>(),
-        _storiesRepository =
-            storiesRepository ?? locator.get<StoriesRepository>(),
         _commentCache = commentCache ?? locator.get<CommentCache>(),
         super(TimeMachineState.init());
 
   final SembastRepository _sembastRepository;
-  final StoriesRepository _storiesRepository;
   final CommentCache _commentCache;
 
   Future<void> activateTimeMachine(Comment comment) async {
     emit(state.copyWith(parents: <Comment>[]));
 
-    final int level = comment.level;
     final List<Comment> parents = <Comment>[];
     Comment? parent = _commentCache.getComment(comment.parent);
     parent ??= await _sembastRepository.getCachedComment(id: comment.parent);
-    parent ??= await _storiesRepository.fetchCommentBy(id: comment.parent);
 
     while (parent != null) {
       parents.insert(0, parent);
 
-      if (parents.length == level) break;
-
       final int parentId = parent.parent;
       parent = _commentCache.getComment(parentId);
       parent ??= await _sembastRepository.getCachedComment(id: parentId);
-      parent ??= await _storiesRepository.fetchCommentBy(id: parentId);
     }
 
     emit(state.copyWith(parents: parents));
