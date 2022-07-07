270279a1b1fc8a312ff0481c0a90ead852bb14f9
Livinglist <georgefung78@live.com>
2022-07-06 20:04:32 -0700

    fixed datetime extension.

--- lib/extensions/context_extension.dart
+++ lib/extensions/context_extension.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-extension TryReadContext on BuildContext {
+extension ContextExtension on BuildContext {
   T? tryRead<T>() {
     try {
       return read<T>();
