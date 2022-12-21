d488352aaef66c44a3e1f3ec0a475873a29ecd12
Livinglist <georgefung78@live.com>
2022-12-20 22:13:36 -0800

    add small.

--- lib/models/font_size.dart
+++ lib/models/font_size.dart
@@ -1,6 +1,7 @@
 import 'package:hacki/styles/styles.dart';
 
 enum FontSize {
+  small('Small', TextDimens.pt15),
   regular('Regular', TextDimens.pt16),
   large('Large', TextDimens.pt17),
   xlarge('XLarge', TextDimens.pt18);
