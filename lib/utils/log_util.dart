a50a0874e7882237c5559ba05400ea0fde16e787
Jiaqi Feng <georgefung78@live.com>
2023-01-29 18:46:55 -0800

    fix logger. (#117)

--- lib/utils/log_util.dart
+++ lib/utils/log_util.dart
@@ -5,6 +5,15 @@ import 'package:hacki/config/constants.dart';
 import 'package:path_provider/path_provider.dart';
 
 abstract class LogUtil {
+  static Future<File> initLogFile() async {
+    final Directory tempDir = await getTemporaryDirectory();
+    final File logFile = File('${tempDir.path}/${Constants.logFilename}');
+    if (logFile.existsSync()) {
+      await logFile.rename('${tempDir.path}/${Constants.previousLogFileName}');
+    }
+    return logFile;
+  }
+
   static Future<File> exportLog() async {
     final Directory tempDir = await getTemporaryDirectory();
     final String logPath = '${tempDir.path}/${Constants.logFilename}';
@@ -16,9 +25,11 @@ abstract class LogUtil {
     final Uint8List fileContent = await currentSessionLog.readAsBytes();
     await previousSessionLog.writeAsString(
       'Current session logs:',
+      mode: FileMode.append,
     );
     return previousSessionLog.writeAsBytes(
       fileContent,
+      mode: FileMode.append,
     );
   }
 }
