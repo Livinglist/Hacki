d22164e2f9ebe170731872d1a541744c40f54835
Livinglist <georgefung78@live.com>
2023-02-11 19:03:30 -0800

    refactor models.

--- lib/models/comment.dart
+++ lib/models/comment.dart
@@ -42,37 +42,5 @@ class Comment extends Item {
   }
 
   @override
-  Map<String, dynamic> toJson() => <String, dynamic>{
-        'id': id,
-        'time': time,
-        'by': by,
-        'text': text,
-        'kids': kids,
-        'parent': parent,
-        'deleted': deleted,
-        'dead': dead,
-        'score': score,
-        'level': level,
-      };
-
-  @override
   bool? get stringify => false;
-
-  @override
-  List<Object?> get props => <Object?>[
-        id,
-        score,
-        descendants,
-        time,
-        by,
-        title,
-        url,
-        kids,
-        dead,
-        parts,
-        deleted,
-        parent,
-        text,
-        type,
-      ];
 }
