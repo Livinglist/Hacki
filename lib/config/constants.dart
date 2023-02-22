7c51bad35e77fc1d0b33f0fde3c75d2d5121327d
Jiaqi Feng <georgefung78@live.com>
2023-02-22 13:37:32 -0800

    add shortcuts for wikipedia and wiktionary. (#157)

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -16,6 +16,8 @@ abstract class Constants {
       'https://news.ycombinator.com/newsguidelines.html';
   static const String githubIssueLink =
       '$githubLink/issues/new?title=Found+a+bug+in+Hacki&body=Please+describe+the+problem.';
+  static const String wikipediaLink = 'https://en.wikipedia.org/wiki/';
+  static const String wiktionaryLink = 'https://en.wiktionary.org/wiki/';
   static const String supportEmail = 'georgefung98@gmail.com';
 
   static const String _imagePath = 'assets/images';
--- lib/extensions/widget_extension.dart
+++ lib/extensions/widget_extension.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:hacki/config/constants.dart';
+import 'package:hacki/models/models.dart';
+import 'package:hacki/screens/widgets/custom_linkify/linkifiers/linkifiers.dart';
+import 'package:hacki/utils/utils.dart';
 
 extension WidgetModifier on Widget {
   Widget padded([EdgeInsetsGeometry value = const EdgeInsets.all(12)]) {
     return Padding(
       padding: value,
       child: this,
+    );
+  }
+
+  Widget contextMenuBuilder(
+    BuildContext context,
+    EditableTextState editableTextState, {
+    required BuildableComment comment,
+  }) {
+    final Iterable<EmphasisElement> emphasisElements =
+        comment.elements.whereType<EmphasisElement>();
+    final int start = editableTextState.textEditingValue.selection.base.offset;
+    final int end = editableTextState.textEditingValue.selection.end;
+
+    final List<ContextMenuButtonItem> items = <ContextMenuButtonItem>[
+      ...editableTextState.contextMenuButtonItems,
+    ];
+
+    if (start != -1 && end != -1) {
+      String selectedText = comment.text.substring(start, end);
+
+      int count = 1;
+      while (selectedText.contains(' ') && count <= emphasisElements.length) {
+        final int s = (start + count * 2).clamp(0, comment.text.length);
+        final int e = (end + count * 2).clamp(0, comment.text.length);
+        selectedText = comment.text.substring(s, e);
+        count++;
+      }
+
+      count = 1;
+      while (selectedText.contains(' ') && count <= emphasisElements.length) {
+        final int s = (start - count * 2).clamp(0, comment.text.length);
+        final int e = (end - count * 2).clamp(0, comment.text.length);
+        selectedText = comment.text.substring(s, e);
+        count++;
+      }
+
+      items.addAll(<ContextMenuButtonItem>[
+        ContextMenuButtonItem(
+          onPressed: () => LinkUtil.launch(
+            '''${Constants.wikipediaLink}$selectedText''',
+          ),
+          label: 'Wikipedia',
+        ),
+        ContextMenuButtonItem(
+          onPressed: () => LinkUtil.launch(
+            '''${Constants.wiktionaryLink}$selectedText''',
+          ),
+          label: 'Wiktionary',
+        ),
+      ]);
+    }
+
+    return AdaptiveTextSelectionToolbar.buttonItems(
+      anchors: editableTextState.contextMenuAnchors,
+      buttonItems: items,
     );
   }
 }
