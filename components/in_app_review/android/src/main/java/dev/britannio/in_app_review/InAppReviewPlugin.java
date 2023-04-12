8559404714ae656ae4d6c451b9e4508577cda268
Livinglist <georgefung78@live.com>
2023-04-11 19:02:20 -0700

    fix in app review for android.

--- components/in_app_review/android/src/main/java/dev/britannio/in_app_review/InAppReviewPlugin.java
+++ components/in_app_review/android/src/main/java/dev/britannio/in_app_review/InAppReviewPlugin.java
@@ -22,17 +22,13 @@ import io.flutter.plugin.common.MethodChannel.Result;
 /**
  * InAppReviewPlugin
  */
-public class InAppReviewPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+public class InAppReviewPlugin implements FlutterPlugin, MethodCallHandler {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
 
-    private Context context;
-    private Activity activity;
-
-    private ReviewInfo reviewInfo;
 
     private final String TAG = "InAppReviewPlugin";
 
@@ -40,13 +36,8 @@ public class InAppReviewPlugin implements FlutterPlugin, MethodCallHandler, Acti
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
         channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "dev.britannio.in_app_review");
         channel.setMethodCallHandler(this);
-        context = flutterPluginBinding.getApplicationContext();
     }
 
-    @Override
-    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-        activity = binding.getActivity();
-    }
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
@@ -62,77 +53,7 @@ public class InAppReviewPlugin implements FlutterPlugin, MethodCallHandler, Acti
     }
 
     @Override
-    public void onDetachedFromActivityForConfigChanges() {
-        onDetachedFromActivity();
-    }
-
-    @Override
-    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-        onAttachedToActivity(binding);
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-        activity = null;
-    }
-
-    @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
         channel.setMethodCallHandler(null);
-        context = null;
     }
-
-    private void isAvailable(final Result result) {
-        return;
-    }
-
-    private void cacheReviewInfo(final Result result) {
-        return;
-    }
-
-    private void requestReview(final Result result) {
-        return;
-    }
-
-    private void launchReviewFlow(final Result result, ReviewManager manager, ReviewInfo reviewInfo) {
-        return;
-    }
-
-    private boolean isPlayStoreInstalled() {
-        return false;
-    }
-
-
-    private void openStoreListing(Result result) {
-        return;
-    }
-
-    private boolean noContextOrActivity() {
-        Log.i(TAG, "noContextOrActivity: called");
-        if (context == null) {
-            Log.e(TAG, "noContextOrActivity: Android context not available");
-            return true;
-        } else if (activity == null) {
-            Log.e(TAG, "noContextOrActivity: Android activity not available");
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private boolean noContextOrActivity(Result result) {
-        Log.i(TAG, "noContextOrActivity: called");
-        if (context == null) {
-            Log.e(TAG, "noContextOrActivity: Android context not available");
-            result.error("error", "Android context not available", null);
-            return true;
-        } else if (activity == null) {
-            Log.e(TAG, "noContextOrActivity: Android activity not available");
-            result.error("error", "Android activity not available", null);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
 }
