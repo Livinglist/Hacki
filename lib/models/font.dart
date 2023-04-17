24090334dbf78fabbb7130f1c4ca2d7a2136416e
Livinglist <georgefung78@live.com>
2023-04-16 18:22:57 -0700

    add font.

--- lib/models/font.dart
+++ lib/models/font.dart
@@ -2,7 +2,8 @@ enum Font {
   roboto('Roboto'),
   robotoSlab('Roboto Slab'),
   ubuntu('Ubuntu'),
-  ubuntuMono('Ubuntu Mono');
+  ubuntuMono('Ubuntu Mono'),
+  notoSerif('Noto Serif');
 
   const Font(this.label);
 
