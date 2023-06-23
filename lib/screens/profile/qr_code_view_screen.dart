5c0e858dee7747443c10c89ae3c37a66cd24c1d0
Livinglist <georgefung78@live.com>
2023-06-22 18:10:46 -0700

    update

--- lib/screens/profile/qr_code_view_screen.dart
+++ lib/screens/profile/qr_code_view_screen.dart
@@ -1,9 +1,5 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:hacki/extensions/context_extension.dart';
 import 'package:hacki/styles/palette.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class QrCodeViewScreen extends StatelessWidget {
@@ -33,28 +29,6 @@ class QrCodeViewScreen extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Palette.transparent,
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.save),
-            onPressed: () async {
-              final QrPainter painter = QrPainter(
-                data: data,
-                version: qrCodeVersion,
-                gapless: true,
-              );
-              final ByteData? imageData = await painter.toImageData(300);
-              if (imageData == null) {
-                return;
-              }
-              await ImageGallerySaver.saveImage(imageData.buffer.asUint8List());
-              if (context.mounted) {
-                context.showSnackBar(
-                  content: 'QR code saved to your photo album.',
-                );
-              }
-            },
-          ),
-        ],
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
