f95b903f57cfa9aa8dcf5dbb4b13b53237d2337c
Livinglist <georgefung78@live.com>
2023-12-02 19:57:20 -0800

    cleanup.

--- lib/styles/theme.dart
+++ lib/styles/theme.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 extension ThemeDataExtension on ThemeData {
   Color get readGrey => colorScheme.onSurface.withOpacity(0.4);
+
+  Color get metadataColor => colorScheme.onSurface.withOpacity(0.6);
 }
