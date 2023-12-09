5a7699d86667c32b79ddea2d43588fbc7e4929c8
Jojo Feng <georgefung78@live.com>
2023-12-09 00:00:20 -0800

    update hacker_news_web_repository.dart (#354)

--- lib/repositories/hacker_news_web_repository.dart
+++ lib/repositories/hacker_news_web_repository.dart
@@ -16,31 +16,17 @@ class HackerNewsWebRepository {
   final Dio _dio;
 
   static const Map<String, String> _headers = <String, String>{
-    'authority': 'www.google.com',
     'accept':
         'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
-    'sec-ch-ua':
-        '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-    'sec-ch-ua-arch': '"x86"',
-    'sec-ch-ua-bitness': '"64"',
-    'sec-ch-ua-full-version': '"115.0.5790.110"',
-    'sec-ch-ua-full-version-list':
-        '"Not/A)Brand";v="99.0.0.0", "Google Chrome";v="115.0.5790.110", "Chromium";v="115.0.5790.110"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-model': '""',
-    'sec-ch-ua-platform': 'Windows',
-    'sec-ch-ua-platform-version': '15.0.0',
-    'sec-ch-ua-wow64': '?0',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-    'x-client-data': '#..',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 17_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Mobile/15E148 Safari/604.1',
   };
 
   static const String _favoritesBaseUrl =
