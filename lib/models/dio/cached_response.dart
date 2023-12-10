6a9132c02d292f0e5ddc0f34a7c614047c79ba75
Livinglist <georgefung78@live.com>
2023-12-10 15:23:35 -0800

    add statuscode.

--- lib/models/dio/cached_response.dart
+++ lib/models/dio/cached_response.dart
@@ -4,12 +4,14 @@ class CachedResponse<T> extends Response<T> {
   CachedResponse({
     required super.requestOptions,
     super.data,
+    super.statusCode,
   }) : setDateTime = DateTime.now();
 
   factory CachedResponse.fromResponse(Response<T> response) {
     return CachedResponse<T>(
       requestOptions: response.requestOptions,
       data: response.data,
+      statusCode: response.statusCode,
     );
   }
 
