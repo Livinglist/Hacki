8d4b2320978794b32f5b5167e7db27371b893505
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2024-08-12 15:00:24 -0700

    fix: migrate uiapplicationmain to main (#443)
    Co-authored-by: Jojo Feng <georgefung78@live.com>

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,7 +6,7 @@ import flutter_secure_storage
 import path_provider_foundation
 import flutter_local_notifications
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
     override func application(
         _ application: UIApplication,
