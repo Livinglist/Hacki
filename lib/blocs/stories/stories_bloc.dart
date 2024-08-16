b76c5dd64c97702e53122051becf258ffc80935a
Jojo Feng <georgefung78@live.com>
2024-08-16 01:29:30 -0700

    fix: manual pagination. (#455)

--- lib/blocs/stories/stories_bloc.dart
+++ lib/blocs/stories/stories_bloc.dart
@@ -109,7 +109,7 @@ class StoriesBloc extends Bloc<StoriesEvent, StoriesState> with Loggable {
       emit(
         state
             .copyWithStoryIdsUpdated(type: type, to: ids)
-            .copyWithCurrentPageUpdated(type: type, to: 0)
+            .copyWithCurrentPageUpdated(type: type, to: 1)
             .copyWithStatusUpdated(type: type, to: Status.inProgress),
       );
 
@@ -125,7 +125,7 @@ class StoriesBloc extends Bloc<StoriesEvent, StoriesState> with Loggable {
     } else {
       emit(
         state
-            .copyWithCurrentPageUpdated(type: type, to: 0)
+            .copyWithCurrentPageUpdated(type: type, to: 1)
             .copyWithStatusUpdated(type: type, to: Status.inProgress),
       );
 
@@ -186,18 +186,11 @@ class StoriesBloc extends Bloc<StoriesEvent, StoriesState> with Loggable {
       ),
     );
 
-    late final int currentPage;
+    final int currentPage = state.currentPageByType[event.type]! + 1;
 
-    /// If useApi is true, it means this is a fallback fetch.
-    /// Don't increment the page number in this case.
-    if (event.useApi) {
-      currentPage = state.currentPageByType[event.type]!;
-    } else {
-      currentPage = state.currentPageByType[event.type]! + 1;
-      emit(
-        state.copyWithCurrentPageUpdated(type: event.type, to: currentPage),
-      );
-    }
+    emit(
+      state.copyWithCurrentPageUpdated(type: event.type, to: currentPage),
+    );
 
     if (state.isOfflineReading) {
       emit(
@@ -218,7 +211,7 @@ class StoriesBloc extends Bloc<StoriesEvent, StoriesState> with Loggable {
         length = ids!.length;
       }
 
-      final int lower = apiPageSize * currentPage;
+      final int lower = min(length, apiPageSize * (currentPage - 1));
       final int upper = min(length, lower + apiPageSize);
       _hackerNewsRepository
           .fetchStoriesStream(
@@ -241,7 +234,15 @@ class StoriesBloc extends Bloc<StoriesEvent, StoriesState> with Loggable {
               case RateLimitedException:
               case RateLimitedWithFallbackException:
               case PossibleParsingException:
+
+                /// Fall back to use API instead.
                 add(event.copyWith(useApi: true));
+                emit(
+                  state.copyWithCurrentPageUpdated(
+                    type: event.type,
+                    to: currentPage - 1,
+                  ),
+                );
             }
           })
           .listen(
