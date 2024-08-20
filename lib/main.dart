823d9f07e366f8b3edfea482203db064b09e5454
Livinglist <georgefung78@live.com>
2024-08-20 16:25:10 -0700

    fix: remove siri suggestion plugin.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_siri_suggestions/flutter_siri_suggestions.dart';
 import 'package:hacki/blocs/blocs.dart';
 import 'package:hacki/config/constants.dart';
 import 'package:hacki/config/custom_router.dart';
@@ -104,16 +103,6 @@ Future<void> main({bool testing = false}) async {
           badge: true,
           sound: true,
         );
-
-    FlutterSiriSuggestions.instance.configure(
-      onLaunch: (Map<String, dynamic> message) async {
-        final String? storyId = message['key'] as String?;
-
-        if (storyId == null) return;
-
-        siriSuggestionSubject.add(storyId);
-      },
-    );
   } else if (Platform.isAndroid) {
     final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
     final AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
