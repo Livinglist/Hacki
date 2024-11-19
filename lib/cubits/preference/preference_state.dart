7d1ee805d6916030bbe07268b0b9a518f6d4587f
livinglist <georgefung78@live.com>
2024-11-18 23:53:02 -0800

    fix: web parser.

--- lib/cubits/preference/preference_state.dart
+++ lib/cubits/preference/preference_state.dart
@@ -90,6 +90,7 @@ class PreferenceState extends Equatable {
   }
 
   HackerNewsDataSource get dataSource {
+    return HackerNewsDataSource.web;
     return HackerNewsDataSource.values.elementAt(
       preferences.singleWhereType<HackerNewsDataSourcePreference>().val,
     );
