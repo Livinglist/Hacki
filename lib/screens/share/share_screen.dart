7b830740302f0dac6c391386104c582a498ae8dd
Jojo F <georgefung78@live.com>
2026-03-08 15:20:44 -0700

    fix: banner text on shared posts. (#556)

--- lib/screens/share/share_screen.dart
+++ lib/screens/share/share_screen.dart
@@ -48,6 +48,7 @@ class _ShareScreenState extends State<ShareScreen> {
   bool _shouldShowParent = true;
   bool _shouldShowHackiBanner = true;
   bool _shouldCopyHnLink = true;
+  bool _shouldScaleText = true;
 
   @override
   Widget build(BuildContext context) {
@@ -116,7 +117,6 @@ class _ShareScreenState extends State<ShareScreen> {
                           children: <Widget>[
                             SizedBoxes.pt8,
                             Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 SizedBoxes.pt12,
                                 Image.asset(
@@ -133,11 +133,19 @@ class _ShareScreenState extends State<ShareScreen> {
                                     color:
                                         Theme.of(context).colorScheme.onSurface,
                                   ),
+                                  textScaler: TextScaler.noScaling,
                                 ),
                               ],
                             ),
                             SizedBoxes.pt8,
-                            targetWidget,
+                            MediaQuery(
+                              data: MediaQuery.of(context).copyWith(
+                                textScaler: _shouldScaleText
+                                    ? null
+                                    : TextScaler.noScaling,
+                              ),
+                              child: targetWidget,
+                            ),
                             SizedBoxes.pt8,
                             if (_shouldShowHackiBanner) ...<Widget>[
                               const Divider(
@@ -145,7 +153,7 @@ class _ShareScreenState extends State<ShareScreen> {
                               ),
                               SizedBoxes.pt8,
                               Text(
-                                '''Shared from Hacki, a third-party open source Hacker News reader.''',
+                                '''Shared from Hacki, an open-source Hacker News reader.''',
                                 style: TextStyle(
                                   fontSize: TextDimens.pt12,
                                   color: Theme.of(context)
@@ -153,6 +161,8 @@ class _ShareScreenState extends State<ShareScreen> {
                                       .onSurface
                                       .withAlpha(150),
                                 ),
+                                textScaler: TextScaler.noScaling,
+                                textAlign: TextAlign.center,
                               ),
                               SizedBoxes.pt8,
                             ],
@@ -216,6 +226,15 @@ class _ShareScreenState extends State<ShareScreen> {
                   onChanged: (_) {
                     setState(() {
                       _shouldCopyHnLink = !_shouldCopyHnLink;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  value: _shouldScaleText,
+                  title: const Text('Text Scaling'),
+                  onChanged: (_) {
+                    setState(() {
+                      _shouldScaleText = !_shouldScaleText;
                     });
                   },
                 ),
