7bd9271365a676e0534b7dca3245bf432039d465
livinglist <georgefung78@live.com>
2026-03-26 14:27:17 -0700

    update

--- lib/screens/widgets/tips/share_screen_tips.dart
+++ lib/screens/widgets/tips/share_screen_tips.dart
@@ -8,7 +8,7 @@ import 'package:hacki/styles/styles.dart';
 class ShareScreenTips extends StatelessWidget {
   const ShareScreenTips({super.key});
 
-  static const double _maxFeatureHintsImageWidth = 200;
+  static const double _maxFeatureHintsImageWidth = 300;
 
   @override
   Widget build(BuildContext context) {
